ff70febdf0c7a4690b7b6965e6326af3c67dc7cd
Bruno Mendoza <hola@brunomendoza.es>
2022-08-03 20:14:03 +0100

    Add login context callback handler

--- src/lab/SampleAcn.java
+++ src/lab/SampleAcn.java
@@ -1,15 +1,14 @@
 package lab;
 
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.callback.*;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 
+/**
+ * // https://docs.oracle.com/javase/1.5.0/docs/guide/security/jaas/tutorials/GeneralAcnOnly.html#MyCallbackHandler
+ */
 class SampleAcn {
-    
-    // https://docs.oracle.com/javase/1.5.0/docs/guide/security/jaas/tutorials/GeneralAcnOnly.html#MyCallbackHandler
 
     public static void main(String[] args) {
         LoginContext loginContext = null;
@@ -29,9 +28,12 @@ class SampleAcn {
         while (!isLogged && count < 3) {
             try {
                 // Carry out the authentication process
+                // If authentication is successful, the SampleLoginModule populates the Subject with a Principal
+                // representing the user.
                 loginContext.login();
                 isLogged = true;
             } catch (LoginException loginException) {
+                count++;
                 System.err.println("Authentication failed" + loginException.getMessage());
 
                 try {
@@ -48,10 +50,26 @@ class SampleAcn {
             System.out.println("You extinguished your tries... :-(");
         }
     }
+
 }
 
+/**
+ * MyCallBackHandler is used to obtain authentication information.
+ * CallbackHandler implementations at com.sun.security.auth.callback
+ * The LoginContext forwards the CallbackHandler directly to the underlying LoginModules.
+ */
 class MyCallBackHandler implements CallbackHandler {
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        for (Callback callback : callbacks) {
+            if (callback instanceof TextOutputCallback) {
 
+            } else if (callback instanceof NameCallback) {
+
+            } else if (callback instanceof PasswordCallback) {
+
+            } else {
+                throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+            }
+        }
     }
 }
